72162c608447bc47db427ea0c045730ae134ba3f
Denis <work80@mail.ru>
2025-12-10 13:47:56 +0700

    [cicd];  was corrected

--- person/views.py
+++ person/views.py
@@ -65,6 +65,6 @@ def main(request):
 
     # GET JS FILES FOR LOGIN AND REGISTER PAGES
     # if "login" in request.path.lower() or "register" in request.path.lower():
-    files = os.listdir(f"{BASE_DIR}/../../collectstatic/scripts")
+    files = os.listdir(f"{BASE_DIR}/collectstatic/scripts")
     files = ["scripts/" + file for file in files]
     return render(request, "index.html", {"js_files": files})
